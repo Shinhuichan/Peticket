9627d80453218a39a7d0cee079078a69d74043a1
Shinhuichan <94081602shc@gmail.com>
2025-07-10 10:38:48 +0900

    v0.0.42 feat: player prefab 변경

--- Assets/3.Script/HuiChan/Object/CollarInteraction.cs
+++ Assets/3.Script/HuiChan/Object/CollarInteraction.cs
@@ -1,3 +1,4 @@
+using CustomInspector;
 using UnityEngine;
 
 
@@ -9,23 +10,33 @@ public enum ObjectType
 public class CollarInteraction : ObjectInteraction
 {
     [SerializeField] ObjectType type;
+    [SerializeField, ReadOnly] bool isEquip = false;
+    AnimalInteraction animal;
+    Collider myCol;
+    void Start()
+    {
+        myCol = transform.GetComponentInChildren<Collider>();
+    }
+    void LateUpdate()
+    {
+        if (isEquip)
+        {
+            // 어떤 target으로 부착되는 지 판단
+            Transform targetTrans = type == ObjectType.Collar ? animal.collarTransform : animal.mouseTransform;
+            myCol.gameObject.transform.position = targetTrans.position;
+            myCol.gameObject.transform.rotation = targetTrans.rotation;
+        }
+    }
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Dog")
         {
             // 필요한 Component들 정의
-            Collider myCol = transform.GetComponentInChildren<Collider>();
+            
             Rigidbody rb = transform.GetComponent<Rigidbody>();
-            AnimalInteraction animal = col.gameObject.GetComponentInParent<AnimalInteraction>();
+            animal = col.gameObject.GetComponentInParent<AnimalInteraction>();
 
-            // 부착됐을 때, 물리 작용 제거
-            myCol.isTrigger = true;
-            rb.isKinematic = true;
-
-            // 어떤 target으로 부착되는 지 판단
-            Transform targetTrans = type == ObjectType.Collar ? animal.collarTransform : animal.mouseTransform;
-            myCol.gameObject.transform.position = targetTrans.position;
-            myCol.gameObject.transform.rotation = targetTrans.rotation;
+            isEquip = true;
         }
     }
 }
