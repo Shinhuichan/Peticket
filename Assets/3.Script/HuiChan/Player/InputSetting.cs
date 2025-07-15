d985ed400733027bbeeaaf1e6e14c5b05fccb3b6
Shinhuichan <94081602shc@gmail.com>
2025-07-15 11:32:01 +0900

    v0.0.54 feat: Inventory Button Test

--- Assets/3.Script/HuiChan/Player/InputSetting.cs
+++ Assets/3.Script/HuiChan/Player/InputSetting.cs
@@ -4,36 +4,36 @@ using UnityEngine.InputSystem;
 public class InputSetting : MonoBehaviour
 {
     public GameObject inventoryPanel;
-    public InputActionReference openMenuAction;
+    public InputActionReference openInventoryAction;
 
     void Start()
     {
-        openMenuAction.action.Enable();
-        openMenuAction.action.performed += ToggleInventory;
+        openInventoryAction.action.Enable();
+        openInventoryAction.action.performed += ToggleInventory;
         InputSystem.onDeviceChange += OnDeviceChange;
     }
     void OnDestroy()
     {
-        openMenuAction.action.Disable();
-        openMenuAction.action.performed -= ToggleInventory;
+        openInventoryAction.action.Disable();
+        openInventoryAction.action.performed -= ToggleInventory;
         InputSystem.onDeviceChange -= OnDeviceChange;
     }
     private void ToggleInventory(InputAction.CallbackContext context)
     {
         inventoryPanel.SetActive(!inventoryPanel.activeSelf);
     }
-    
+
     void OnDeviceChange(InputDevice device, InputDeviceChange change)
     {
         switch (change)
         {
             case InputDeviceChange.Disconnected:
-                openMenuAction.action.Disable();
-                openMenuAction.action.performed -= ToggleInventory;
+                openInventoryAction.action.Disable();
+                openInventoryAction.action.performed -= ToggleInventory;
                 break;
             case InputDeviceChange.Reconnected:
-                openMenuAction.action.Enable();
-                openMenuAction.action.performed += ToggleInventory;
+                openInventoryAction.action.Enable();
+                openInventoryAction.action.performed += ToggleInventory;
                 break;
         }
     }
