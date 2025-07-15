3107c277f48a26c57649eb25c5c18dece59d198b
Shinhuichan <94081602shc@gmail.com>
2025-07-15 12:36:45 +0900

    v0.0.56 edit: Dash 기능 구현(Test 필요)

--- Assets/3.Script/HuiChan/Player/InputSetting.cs
+++ Assets/3.Script/HuiChan/Player/InputSetting.cs
@@ -1,39 +1,81 @@
+using CustomInspector;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class InputSetting : MonoBehaviour
 {
+    [Header("Inventory Setting")]
     public GameObject inventoryPanel;
-    public InputActionReference openInventoryAction;
+    public InputActionReference toggleInventoryAction;
+
+    [Header("Dash Setting")]
+    [SerializeField] float dashIncrease = 1.5f;
+    [SerializeField, ReadOnly] float baseMoveSpeed;
+    public InputActionReference dashAction;
+    public DynamicMoveProvider moveProvider;
 
     void Start()
     {
-        openInventoryAction.action.Enable();
-        openInventoryAction.action.performed += ToggleInventory;
+        moveProvider = FindFirstObjectByType<DynamicMoveProvider>();
+        if (moveProvider == null)
+        {
+            Debug.LogWarning($"Input Setting | moveProvider가 Null입니다.");
+            return;
+        }
+        baseMoveSpeed = moveProvider.moveSpeed;
+        toggleInventoryAction.action.Enable();
+        dashAction.action.Enable();
+
+        toggleInventoryAction.action.performed += ToggleInventory;
+        dashAction.action.performed += Dash;
+        dashAction.action.canceled += DashStop;
         InputSystem.onDeviceChange += OnDeviceChange;
     }
     void OnDestroy()
     {
-        openInventoryAction.action.Disable();
-        openInventoryAction.action.performed -= ToggleInventory;
+        toggleInventoryAction.action.Disable();
+        dashAction.action.Disable();
+
+        toggleInventoryAction.action.performed -= ToggleInventory;
+        dashAction.action.performed -= Dash;
+        dashAction.action.canceled -= DashStop;
         InputSystem.onDeviceChange -= OnDeviceChange;
     }
+
+    #region Dash
+    private void Dash(InputAction.CallbackContext context)
+    {
+        // Dash Start 기능
+        moveProvider.moveSpeed = baseMoveSpeed * dashIncrease;
+    }
+    private void DashStop(InputAction.CallbackContext context)
+    {
+        // Dash Stop 기능
+        moveProvider.moveSpeed = baseMoveSpeed;
+    }
+    #endregion
+
+    #region Inventory
     private void ToggleInventory(InputAction.CallbackContext context)
     {
         inventoryPanel.SetActive(!inventoryPanel.activeSelf);
     }
+    #endregion
+
+
 
     void OnDeviceChange(InputDevice device, InputDeviceChange change)
     {
         switch (change)
         {
             case InputDeviceChange.Disconnected:
-                openInventoryAction.action.Disable();
-                openInventoryAction.action.performed -= ToggleInventory;
+                toggleInventoryAction.action.Disable();
+                toggleInventoryAction.action.performed -= ToggleInventory;
                 break;
             case InputDeviceChange.Reconnected:
-                openInventoryAction.action.Enable();
-                openInventoryAction.action.performed += ToggleInventory;
+                toggleInventoryAction.action.Enable();
+                toggleInventoryAction.action.performed += ToggleInventory;
                 break;
         }
     }
