c1ff4c3dac6a0104214b79cf523e559c9cdbc4f6
Shinhuichan <94081602shc@gmail.com>
2025-07-16 16:01:32 +0900

    v0.0.71 feat: 인벤토리 UI가 활성화된 상태에서 비활성화시키는 Action 추가

--- Assets/1.Scene/Gunmo/Script/VRInventoryController.cs
+++ Assets/1.Scene/Gunmo/Script/VRInventoryController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XR;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class VRInventoryController : MonoBehaviour
 {
@@ -8,13 +9,33 @@ public class VRInventoryController : MonoBehaviour
 
     public InputActionReference moveAction;   // 휠이나 스틱 좌우
     public InputActionReference selectAction; // 버튼 (예: 트리거)
+    public InputActionAsset playerInputActions;
+    private InputActionMap leftInteractionActionMap;
+    private InputActionMap rightInteractionActionMap;
+    private InputActionMap leftLocomotionActionMap;
+    private InputActionMap rightLocomotionActionMap;
+    [SerializeField] InputActionReference leftHandMoveAction;
+    private InputActionMap inventoryActionMap;
+    public GameObject inventoryPanel;
+    public InputActionReference toggleInventoryAction;
+     DynamicMoveProvider moveProvider;
 
     private void OnEnable()
     {
         moveAction.action.performed += OnMove;
         selectAction.action.performed += OnSelect;
         moveAction.action.Enable();
+
         selectAction.action.Enable();
+        toggleInventoryAction.action.Enable();
+        toggleInventoryAction.action.performed += ToggleInventory;
+
+        leftInteractionActionMap = playerInputActions.FindActionMap("XRI LeftHand Interaction");
+        rightInteractionActionMap = playerInputActions.FindActionMap("XRI RightHand Interaction");
+        leftLocomotionActionMap = playerInputActions.FindActionMap("XRI LeftHand Locomotion");
+        rightLocomotionActionMap = playerInputActions.FindActionMap("XRI RightHand Locomotion");
+        inventoryActionMap = playerInputActions.FindActionMap("XRI Inventory");
+        moveProvider = FindFirstObjectByType<DynamicMoveProvider>();
     }
 
     private void OnDisable()
@@ -23,6 +44,9 @@ public class VRInventoryController : MonoBehaviour
         selectAction.action.performed -= OnSelect;
         moveAction.action.Disable();
         selectAction.action.Disable();
+        selectAction.action.Disable();
+        toggleInventoryAction.action.Disable();
+        toggleInventoryAction.action.performed -= ToggleInventory;
     }
 
     private void OnMove(InputAction.CallbackContext context)
@@ -43,4 +67,42 @@ public class VRInventoryController : MonoBehaviour
     {
         highlighter.SelectCurrentSlot();
     }
+    #region OpenInventory
+    private void ToggleInventory(InputAction.CallbackContext context)
+    {
+        bool isActive = !inventoryPanel.activeSelf;
+        inventoryPanel.SetActive(isActive);
+
+        if (isActive)
+        {
+            // 인벤토리 열림: 이동과 상호작용 모두 비활성화, 인벤토리만 활성화
+            leftInteractionActionMap?.Disable();
+            rightInteractionActionMap?.Disable();
+            leftLocomotionActionMap?.Disable();
+            rightLocomotionActionMap?.Disable();
+            inventoryActionMap?.Enable();
+
+            // 이동 InputAction 자체를 Disable
+            leftHandMoveAction?.action.Disable();
+            if (moveProvider != null) moveProvider.enabled = false;
+
+            Debug.Log("인벤토리 열림: 이동/상호작용 비활성화, 인벤토리 액션맵 활성화");
+        }
+        else
+        {
+            // 인벤토리 닫힘: 다시 이동/상호작용 활성화
+            inventoryActionMap?.Disable();
+            leftInteractionActionMap?.Enable();
+            rightInteractionActionMap?.Enable();
+            leftLocomotionActionMap?.Enable();
+            rightLocomotionActionMap?.Enable();
+
+            // 이동 InputAction 자체를 Enable
+            leftHandMoveAction?.action.Enable();
+            if (moveProvider != null) moveProvider.enabled = true;
+
+            Debug.Log("인벤토리 닫힘: 인벤토리 액션맵 비활성화, 이동/상호작용 활성화");
+        }
+    }
+    #endregion
 }
