8f42328b17bcf7cbce8108f5533b3cfcf6235e74
Dexmore <110244668+Dexmore@users.noreply.github.com>
2025-07-21 17:32:34 +0900

    v0.0.94 feat.Setting prefab

--- Assets/3.Script/Soohyun/Sound/AudioManager.cs
+++ Assets/3.Script/Soohyun/Sound/AudioManager.cs
@@ -11,14 +11,8 @@ public class AudioManager : MonoBehaviour
     public AudioSource bgmSource_bgm;
     public Slider volumeSlider_bgm;
 
-    [Header("Hit Sound")]
-    public AudioSource bgmSource_hit;
-    public Slider volumeSlider_hit;
-
-    [Header("Run Sound")]
-    public AudioSource bgmSource_run;
-    public Slider volumeSlider_run;
-
+    [Header("Button")]
+    public Button BackButton;
     void Awake()
     {
         // 싱글톤 처리
@@ -41,25 +35,16 @@ public class AudioManager : MonoBehaviour
         bgmSource_bgm.volume = savedBgm;
         if (volumeSlider_bgm != null) volumeSlider_bgm.value = savedBgm;
 
-        float savedHit = PlayerPrefs.GetFloat("Hit", 1.0f);
-        bgmSource_hit.volume = savedHit;
-        if (volumeSlider_hit != null) volumeSlider_hit.value = savedHit;
-
-        float savedRun = PlayerPrefs.GetFloat("Run", 1.0f);
-        bgmSource_run.volume = savedRun;
-        if (volumeSlider_run != null) volumeSlider_run.value = savedRun;
 
         // 슬라이더 이벤트 연결
         if (volumeSlider_bgm != null)
             volumeSlider_bgm.onValueChanged.AddListener(OnBgmVolumeChange);
-        if (volumeSlider_hit != null)
-            volumeSlider_hit.onValueChanged.AddListener(OnHitVolumeChange);
-        if (volumeSlider_run != null)
-            volumeSlider_run.onValueChanged.AddListener(OnRunVolumeChange);
 
         // BGM 재생 (중복 방지)
         if (!bgmSource_bgm.isPlaying)
             bgmSource_bgm.Play();
+
+        BackButton.onClick.AddListener(Back);
     }
 
     void OnBgmVolumeChange(float value)
@@ -69,17 +54,8 @@ public class AudioManager : MonoBehaviour
         PlayerPrefs.Save();
     }
 
-    void OnHitVolumeChange(float value)
+    public void Back()
     {
-        bgmSource_hit.volume = value;
-        PlayerPrefs.SetFloat("Hit", value);
-        PlayerPrefs.Save();
-    }
 
-    void OnRunVolumeChange(float value)
-    {
-        bgmSource_run.volume = value;
-        PlayerPrefs.SetFloat("Run", value);
-        PlayerPrefs.Save();
     }
 }
