05943426e70221e6011abea8d073819cdf218e0d
Dexmore <110244668+Dexmore@users.noreply.github.com>
2025-07-23 17:11:08 +0900

    v0.0.143 feat. Animal_AudioSource

---
+++ Assets/3.Script/Soohyun/Sound/AudioSourceInventory.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceInventory : MonoBehaviour
+{
+    [Header("Bark Sounds")]
+    public AudioSource[] BarkSound;
+
+    [Header("Eat Sound")]
+    public AudioSource EatSound;
+
+    [Header("Walk Sound")]
+    public AudioSource WalkSound;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
