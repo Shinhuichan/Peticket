b68ebaaf37c6e4e9107f81ae33b6951865ef079f
Dexmore <110244668+Dexmore@users.noreply.github.com>
2025-07-24 16:47:19 +0900

    v 0.0.165 Feat : Object Interation Sound Make

---
+++ Assets/2.Model/Prefabs/SooHyun/Audio/AudioInteraction.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioInteraction : MonoBehaviour
+{
+    [Header("Ball Narration")]
+    public AudioClip[] Ball_N;
+
+    [Header("Leash Narration")]
+    public AudioClip[] Leash_N;
+
+    [Header("Muzzle Narration")]
+    public AudioClip[] Muzzle_N;
+
+    [Header("Shovel Narration")]
+    public AudioClip[] Shovel_N;
+
+    [Header("Snack Narration")]
+    public AudioClip[] Snack_N;
+}
